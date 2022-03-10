26003a49ebbbe900fefdb869c8028ece190ec3b3
hackerdroidofficial <96161506+hackerdroidofficial@users.noreply.github.com>
2022-03-10 21:35:37 +0530

    Create Jdk8.sh

---
+++ Jdk8.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pkg install figlet
+pkg install wget
+echo "java jdk8 installing now"
+wget https://github.com/hackerdroidofficial/javajdk/releases/download>
+dpkg -i openjdk-8-jre-headless_8u252-b09-1~deb9u1_armhf.deb
+figlet done
